46d88937e39922cf9b253b215b7dbff2e830fb52
Yauheni Aviltsau <xopek-007@yandex.ru>
2023-07-26 07:07:25 +0300

    home work is done

--- Sem02_Exceptions_25072023/src/ClassWork/Main3.java
+++ Sem02_Exceptions_25072023/src/ClassWork/Main3.java
@@ -7,7 +7,7 @@ public class Main3 {
     Можно внести в код правки, которые считаете необходимыми.
      */
     public static void main(String[] args) {
-        String[][] testArr= new String[][]{{"1","a","3"},{"0","o","4"}};
+        String[][] testArr= new String[][]{{"1","$","3"},{"0","o","4"}};
         System.out.println(ex2(testArr));
     }
     public static int ex2(String[][] arr){
@@ -15,8 +15,8 @@ public class Main3 {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 String item = arr[i][j];
-                //if (item.matches("[0-9]+")){
-                if (checkText(item)) {
+                if (item.matches("[0-9]+")){
+//  вернуть              if (checkText(item)) {
                     int val = Integer.parseInt(arr[i][j]);
                     sum += val;
                 }
@@ -29,6 +29,7 @@ public class Main3 {
     private static boolean checkText(String text){
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) < '0' || text.charAt(i) > '9'){
+                System.out.println(i + " text " + text + " text.charAt(i): " + text.charAt(i));
                 return false;
             }
         }
